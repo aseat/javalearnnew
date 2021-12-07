cb3fffc26d6b2eba171a6201672d18c32dc0611a
user <user@DESKTOP-QT28T8M>
2021-12-07 21:55:55 +0900

    繰り返し分および配列の復習

---
+++ Mytest1/src/java3/Code3_2.java
@@ -0,0 +1,11 @@
+package java3;
+
+public class Code3_2 {
+	public static void main(String[] args) {
+		boolean doorClose = true;
+		while(doorClose) {
+			System.out.print("ƒmƒbƒN‚·‚é");
+			System.out.print("1•ª‘Ò‚Â");
+		}
+	}
+}
---
+++ Mytest1/src/java4/Code4_4.java
@@ -0,0 +1,8 @@
+package java4;
+
+public class Code4_4 {
+	public static void main(String[] args) {
+		int [] scores = new int[5];
+		System.out.println(scores.length);
+	}
+}
